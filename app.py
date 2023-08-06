be236de986438e891feabc05808b9b89590bcf47
tekelala <camilo@tekelala.com>
2023-08-06 16:24:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,19 +27,22 @@ def app():
             if st.button('Generar Mercado', key='boton_generar_mercado'):
                 # Call your function here
                 generar_desde_mercado(mercado_proyecto)
+                st.session_state.container_1 = True
 
         elif option == "El interés de solucionar un problema particular":
             problema_proyecto = st.text_input("¿Cuál problema?")
             if st.button('Generar Problema', key='boton_generar_problema'):
                 # Call your function here
                 generar_desde_problema(problema_proyecto)
+                st.session_state.container_1 = True
 
         elif option == "No tengo aún nada definido":
             if st.button('Generar Azar', key='boton_generar_azar'):
                 # Call your function here
                 generar_desde_azar(azar_proyecto)
+                st.session_state.container_1 = True
 
-    if option != "":
+    if 'container_1' in st.session_state and st.session_state.container_1:
         with st.container():
             st.markdown("¿Tienes definida la propuesta de valor?")
             option_propuesta_valor = st.selectbox('Selecciona una opción', ["", "Sí", "No"])
@@ -49,20 +52,23 @@ def app():
                 if st.button('Generar Propuesta de Valor', key='boton_generar_propuesta_valor'):
                     # Call your function here
                     generar_prop_valor_usuario(propuesta_valor_proyecto)
+                    st.session_state.container_2 = True
 
             elif option_propuesta_valor == "No":
                 if st.button('Generar Propuesta de Valor sin Definir', key='boton_generar_propuesta_valor_0'):
                     # Call your function here
                     generar_propvalor()
+                    st.session_state.container_2 = True
 
-        if option_propuesta_valor != "":
+        if 'container_2' in st.session_state and st.session_state.container_2:
             with st.container():
                 st.markdown("Generar un modelo de negocio")
                 if st.button('Generar Modelo de Negocio', key='boton_generar_modelo_negocio'):
                     # Call your function here
                     generar_modelo_negocio()
+                    st.session_state.container_3 = True
 
-            if st.button('Generar Modelo de Negocio'):
+            if 'container_3' in st.session_state and st.session_state.container_3:
                 with st.container():
                     st.markdown("Generar un pitch deck")
                     if st.button('Generar Pitch Deck', key='boton_generar_pitch_deck'):
