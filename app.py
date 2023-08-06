16471650670b2ecf61434f823cc119826ee9697b
tekelala <camilo@tekelala.com>
2023-08-06 16:14:45 -0500

    working

--- app.py
+++ app.py
@@ -39,42 +39,35 @@ def app():
                 # Call your function here
                 generar_desde_azar(azar_proyecto)
 
-    # Create an empty placeholder for the container
-    propuesta_valor_container = st.empty()
+    if option != "":
+        with st.container():
+            st.markdown("¿Tienes definida la propuesta de valor?")
+            option_propuesta_valor = st.selectbox('Selecciona una opción', ["", "Sí", "No"])
 
-    with st.container():
-        
-        st.markdown("¿Tienes definida la propuesta de valor?")
-        option_propuesta_valor = st.selectbox('Selecciona una opción', ["", "Sí", "No"])
+            if option_propuesta_valor == "Sí":
+                propuesta_valor_proyecto = st.text_input("¿Cuál es?")
+                if st.button('Generar Propuesta de Valor', key='boton_generar_propuesta_valor'):
+                    # Call your function here
+                    generar_prop_valor_usuario(propuesta_valor_proyecto)
 
-        if option_propuesta_valor == "Sí":
-            propuesta_valor_proyecto = st.text_input("¿Cuál es?")
-            if st.button('Generar Propuesta de Valor', key='boton_generar_propuesta_valor'):
-                # Call your function here
-                generar_prop_valor_usuario(propuesta_valor_proyecto)
+            elif option_propuesta_valor == "No":
+                if st.button('Generar Propuesta de Valor sin Definir', key='boton_generar_propuesta_valor_0'):
+                    # Call your function here
+                    generar_propvalor()
 
-        elif option_propuesta_valor == "No":
-            if st.button('Generar Propuesta de Valor sin Definir', key='boton_generar_propuesta_valor_0'):
-                # Call your function here
-                generar_propvalor()
+        if option_propuesta_valor != "":
+            with st.container():
+                st.markdown("Generar un modelo de negocio")
+                if st.button('Generar Modelo de Negocio', key='boton_generar_modelo_negocio'):
+                    # Call your function here
+                    generar_modelo_negocio()
 
-    # Create an empty placeholder for the container
-    modelo_negocio_container = st.empty()
-
-    with st.container():
-        st.markdown("Generar un modelo de negocio")
-        if st.button('Generar Modelo de Negocio', key='boton_generar_modelo_negocio'):
-            # Call your function here
-            generar_modelo_negocio()
-
-    # Create an empty placeholder for the container
-    pitch_deck_container = st.empty()
-
-    with st.container():
-        st.markdown("Generar un pitch deck")
-        if st.button('Generar Pitch Deck', key='boton_generar_pitch_deck'):
-            # Call your function here
-            generar_pitchdeck()
+            if st.button('Generar Modelo de Negocio'):
+                with st.container():
+                    st.markdown("Generar un pitch deck")
+                    if st.button('Generar Pitch Deck', key='boton_generar_pitch_deck'):
+                        # Call your function here
+                        generar_pitchdeck()
         
 if __name__ == "__main__":
     app()
