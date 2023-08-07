1db3c40895951a90fe2531a2874e320f85fe3810
tekelala <camilo@tekelala.com>
2023-08-06 19:21:56 -0500

    change

--- functions.py
+++ functions.py
@@ -1,61 +1,15 @@
 import streamlit as st
-import requests
-import json
 
-# Initialize session state variables
-if "result" not in st.session_state:
-    st.session_state.result = ""
-
-# Claude functions
-def create_text(prompt):
-    api_url = "https://api.anthropic.com/v1/complete"
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-Key": st.secrets["API_KEY"]  # Use the API key from Streamlit's secrets
-    }
-
-    # Prepare the prompt for Claude
-    conversation = f"Human: {prompt}\n\nAssistant:"
-
-    # Define the body of the request
-    body = {
-        "prompt": conversation,
-        "model": "claude-2.0",
-        "temperature": 0,
-        "max_tokens_to_sample": 10000,
-        "stop_sequences": ["\n\nHuman:"]
-    }
-
-    # Make a POST request to the Claude API
-    try:
-        response = requests.post(api_url, headers=headers, data=json.dumps(body))
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        st.error(f"HTTP Error: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        st.error(f"Error Connecting: {errc}")
-    except requests.exceptions.Timeout as errt:
-        st.error(f"Timeout Error: {errt}")
-    except requests.exceptions.RequestException as err:
-        st.error(f"Something went wrong: {err}")
-    except Exception as e:
-        st.error(f"Unexpected error: {e}")
-
-    # Extract Claude's response from the JSON response
-    result = response.json()
-
-    # Return Claude's response as a string
-    return result['completion']
-
-# Function to create the prompt for the decks generation
-def prompt_compliance(text_file):
-    prompts = f'''Role: You are Nancy Duarte part of the Kravata team an expert in crafting slide Decks for startups.'''
-
-    return prompts
 
 
 def generar_desde_mercado(mercado_proyecto):
-    prompts = f'''Role: You are Nancy Duarte part of the Kravata team an expert in crafting slide Decks for startups.'''
+    prompts = f'''Role: You are top entrepreneur. Taking as input the following market: {mercado_proyecto} do the following tasks and answer always in Spanish. 
+    Task 1: write "Mercado:"
+    Task 2: describe {mercado_proyecto}
+    Task 3: write "Problema"
+    Task 4: describe the problem with the biggest potential market for a undergraduate student. 
+    Task 5: enumerate and describe each potential customer
+    Task 6: using the Jobs to be done describe the job to be done for each potential customer defined in the previous task'''
 
     return prompts
 
@@ -85,43 +39,4 @@ def generar_pitchdeck():
 
 
 
-text_file = ""
 
-# Initialize session state variables
-if "result" not in st.session_state:
-    st.session_state.result = ""
-
-
-
-st.title('Compliance')
-
-# Initialize session state variables if not already done
-if "result" not in st.session_state:
-    st.session_state.result = ""
-if "prompts" not in st.session_state:
-    st.session_state.prompts = ""
-
-
-
-if st.button('Create'):
-    with st.spinner('Writting...'):
-        # Create the 'prompts' variable
-        st.session_state.prompts = prompt_compliance(text_file)
-
-        # Call the 'send_message()' function with the 'prompts' variable
-        st.session_state.result = create_text(st.session_state.prompts)
-
-        # Display the prompt
-        #st.write(st.session_state.prompts)
-        # Display the result
-        st.write(st.session_state.result)
-
-# Allow the user to propose changes
-if st.session_state.result != "":
-    user_changes = st.text_input('Propose changes to the deck:')
-    if st.button('Apply Changes'):
-        if user_changes:
-            st.session_state.prompts += f" Please change the communications piece with the following instructions: {user_changes.strip()}"
-            with st.spinner('Applying changes...'):
-                st.session_state.result = create_text(st.session_state.prompts)
-            st.write(st.session_state.result)
